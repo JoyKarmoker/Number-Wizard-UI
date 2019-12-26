27c32c4017e83a3b87e252e40abb9ba2f996acfb
Joy Karmoker <joykarmoker353@gmail.com>
2019-12-27 01:39:47 +0600

    Sucees button is to be changed

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        //StartGame();
+
+        NextGuess();
+        max = max + 1;
+
+    }
+
+   /* void StartGame()
+    {
+
+       
+
+       Debug.Log("G'day mate, welcome to numba wiz-ud...");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        
+    }*/
+
+
+    public void OnPressHigher()
+    {
+        min = guess+1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess-1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
+
+
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentsceneindex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentsceneindex + 1);
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
